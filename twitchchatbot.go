6e9a23bb69bd82bf3bdee49a8e73e65249d6ccac
alexander-e-andrews <alexander.e.andrews.ce@gmail.com>
2020-08-29 16:07:38 -0400

    Changed the handler to a simpliler system

--- twitchchatbot.go
+++ twitchchatbot.go
@@ -6,35 +6,41 @@ import (
 
 	"gopkg.in/irc.v3"
 )
+
 //Im just generating oauth from https://twitchapps.com/tmi/
 //Need to add the auto-functionality
 
 //Result is a struct for a channel to return, incase the Bot has an error
-type Result struct{
-	Source string //The bot that called the error
+type Result struct {
+	Source  string //The bot that called the error
 	Message string //Message to explain where error occurred
-	Error error //The error itself
+	Error   error  //The error itself
 }
+
 //ErrorChannel is a channel you should listen to for any errors that come back across the bots
 var ErrorChannel chan Result
 
 //On init, allocate the channel
-func init(){
+func init() {
 	ErrorChannel = make(chan Result)
 }
 
-// BasicBot is a basic bot taht connects to twitch
+// BasicBot is a basic bot that connects to twitch
+// Set ID, Nickname, Password, Channel, and ReceiveMessage before calling ConnectToChat. 
+//Listin on HasJoined to know when the bot has successfully connected to the chat room
 type BasicBot struct {
-	ID string //An ID given to the bot to track its progression
-	Nickname string
-	Password string //oath:... //This will be replaced at some point with an automatic login function
-	Handler  func(*irc.Client, *irc.Message) //Your functionality that you want to handle
-	Client   *irc.Client
-	Channel  string //The username of the chat channel you wish to connect to
+	ID             string //An ID given to the bot to track its progression
+	Nickname       string
+	Password       string                          //oath:... //This will be replaced at some point with an automatic login function
+	Channel        string                          //The username of the chat channel you wish to connect to
+	ReceiveMessage func(username, message string)  //A simpler handler, only gives relevant string information. Call bot.SendMessage to reply
+	client         *irc.Client
+	HasJoined      chan bool //Becomes true when a bot has successfully joined the channel
 }
 
 // ConnectToChat connects the bot to the chatroom
 func (b *BasicBot) ConnectToChat() {
+	b.HasJoined = make(chan bool)
 	//This is the unsecured line, follow https://dave.cheney.net/2010/10/05/how-to-dial-remote-ssltls-services-in-go
 	//to see if we can get that secure connection working
 	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
@@ -44,13 +50,13 @@ func (b *BasicBot) ConnectToChat() {
 	config := irc.ClientConfig{
 		Nick:    b.Nickname,
 		Pass:    b.Password,
-		Handler: irc.HandlerFunc(outsideHandler(b.Handler, b.Channel)),
+		Handler: irc.HandlerFunc(outsideHandler(b)),
 	}
 
-	b.Client = irc.NewClient(conn, config)
+	b.client = irc.NewClient(conn, config)
 
-	go func(){
-		err := b.Client.Run()
+	go func() {
+		err := b.client.Run()
 		rs := Result{Error: err, Source: b.ID, Message: "Error running the client"}
 		ErrorChannel <- rs
 	}()
@@ -61,26 +67,30 @@ func (b *BasicBot) ConnectToChat() {
 func (b *BasicBot) SendMessage(msg string) {
 	fmsg := irc.Message{}
 	fmsg.Command = "PRIVMSG"
-	fmsg.Params = []string{"#"+b.Channel, msg}
-	err := b.Client.WriteMessage(&fmsg)
+	fmsg.Params = []string{"#" + b.Channel, msg}
+	err := b.client.WriteMessage(&fmsg)
 	rs := Result{Error: err, Source: b.ID, Message: "Error sending message"}
 	ErrorChannel <- rs
 }
 
-func outsideHandler(h func(*irc.Client, *irc.Message), channel string) func(c *irc.Client, m *irc.Message) {
+func outsideHandler(b *BasicBot) func(c *irc.Client, m *irc.Message) {
 	return func(c *irc.Client, m *irc.Message) {
-		if m.Command == "PING" {
+		//I think this is good since we won't receiver this until we actually join
+		if m.Command == "PRIVMSG" {
+			b.ReceiveMessage(m.Name, m.Params[1])
+			return
+		} else if m.Command == "PING" {
 			msg := irc.Message{}
 			msg.Command = "PONG"
 			msg.Params = append(msg.Params, "tmi.twitch.tv")
 			c.WriteMessage(&msg)
 			return
-		}else if m.Command == irc.RPL_ENDOFMOTD {
-			c.Write("JOIN #"+ channel)
-			fmt.Println("Joining: ", channel)
+		} else if m.Command == irc.RPL_ENDOFMOTD {
+			c.Write("JOIN #" + b.Channel)
+			//fmt.Println("Joining: ", b.Channel)
 			return
-		}else{
-			h(c, m)
+		} else if m.Command == irc.RPL_ENDOFNAMES {
+			b.HasJoined <- true
 		}
 	}
 }
